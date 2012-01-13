ee45068fe92b74a510163a83a01ade7f2b89be5d
Todd Bodnar <tjb5215@psu.edu>
2012-01-13 08:40:23 -0500

    updated edge list
    to correctly work (issue from confusion between tweets, tweeters, etc.)

--- Data/tweets/h1n1/make_edge_list.py
+++ Data/tweets/h1n1/make_edge_list.py
@@ -1,14 +1,32 @@
-# Pipe in reindexed-graph.txt
 # Outputs edge list of friends
 
 import sys
+import random
 
-for line in sys.stdin:
+users = [] # list of tweeter_ids
+tweets = {} #map of list of tweets for each tweeter
+
+for line in open('reindexed-tweets.txt','r'):
+    sp = line.split('\t')
+    uid = sp[2].strip()
+    if tweets.get(uid,None) == None:
+        users = users + [uid]
+        tweets[uid] = []
+    tweets[uid] = tweets[uid]+[sp[0].strip()]
+
+
+for line in open('reindexed-graph.txt','r') :
     sp = line.split('|')
+    tweetid = sp[0].strip()
     if sp[1] == 'FR':
         friends = sp[2].split(',')
-        for fr in friends:
-            sys.stdout.write(sp[0])
-            sys.stdout.write(';')
-            sys.stdout.write(fr.strip())
-            sys.stdout.write('\n')
+        uid = ''
+        for user in users:
+            if tweetid in tweets[user]:
+                for fr in friends :
+                    sys.stdout.write(sp[0])
+                    sys.stdout.write(';')
+                    sys.stdout.write(fr.strip())
+                    sys.stdout.write(';undirected\n')
+                    break
+                    
